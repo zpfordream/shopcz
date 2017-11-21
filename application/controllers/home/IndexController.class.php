9e4b7fab81e699ada5d14c709012b175eb457e79
zpmff <435649398@qq.com>
2017-11-21 12:57:03 +0800

    seond， add front page

---
+++ application/controllers/home/IndexController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 益彩通
+ * Date: 2017-11-21
+ * Time: 12:52
+ */
+
+
+class IndexController extends Controller{
+
+    public function indexAction(){
+
+        include CUR_VIEW_PATH."index.html";
+    }
+
+
+}
