85ddfca0d2b1f55ae4ccf104c63ab35ffb45d797
zpmff <435649398@qq.com>
2017-12-04 18:12:05 +0800

    at company rbac 12%

--- application/models/UserModel.class.php
+++ application/models/UserModel.class.php
@@ -13,10 +13,45 @@ class UserModel extends Model{
 //        $sql = "select * from {$this->table}";
 //        $users =  $this->db->getAll($sql);
 //        return $users;
+        //user表和关联表，连表查询
 
         $sql = "select a.user_id ,a.user_name,a.email,a.status,a.reg_time,a.last_login_time,b.groud_id from {$this->table} AS a JOIN cz_auth_group_id AS b ON a.user_id = b.uid ";
         $users = $this->db->getAll($sql);
-        return $users;
+
+        $first = $users[0];
+        $first['title'] = array();
+        $user_data[$first['id']] = $first;
+
+        //组合数组，把id重复的数组过滤掉
+        foreach( $users as $key => $value){
+
+            foreach( $user_data as $m => $n){
+
+                $ids = array_map(function($a){ return $a['id'];} , $user_data );
+                if(!in_array( $value['id'],$ids)){
+
+                    $value['title'] = array();
+                    $user_data[$value['id']] = $value ;
+                }
+            }
+        }
+
+        //组合管理员title的数组
+        foreach( $user_data as $key => $value){
+
+            foreach( $users as $m =>$n ){
+
+                if( $n['id'] == $key){
+
+                    $user_data[$key]['title'][] = $n['title']  ;
+                }
+            }
+            $user_data[$key]['title'] = implode( "、" , $user_data[$key]['title'] );
+        }
+
+        return $user_data ;
+
+//        return $users;
     }
 
 }
